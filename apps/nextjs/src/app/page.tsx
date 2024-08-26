0f6ba8fae36551140be9b5d5d2de14a147a2458a
meetqy <meetqy@icloud.com>
2024-08-26 22:35:23 +0800

    genshin impact

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,5 +1,4 @@
 import GenshinImpactCharacter from "@randomkits/genshin-impact-character";
-import { getAssetsSrc } from "@randomkits/utils";
 
 export default function HomePage() {
   const data = GenshinImpactCharacter.random({ num: 6, language: "en" });
@@ -10,7 +9,7 @@ export default function HomePage() {
         <div key={item.name}>
           <h1>{item.name}</h1>
           <p>{item.element}</p>
-          <img src={getAssetsSrc("genshin-impact-character", item.name)} />
+          <img src={item.avatar} />
         </div>
       ))}
     </div>
--- kits/genshin-impact-character/src/index.ts
+++ kits/genshin-impact-character/src/index.ts
@@ -1,16 +1,29 @@
-import { randoms } from "@randomkits/utils";
+import { getAssetsSrc, randoms } from "@randomkits/utils";
 
 import en from "../assets/data/en.json";
+
+type GenshinImpactCharacter = (typeof en)[number] & {
+  avatar: string;
+};
 
 const GenshinImpactCharacter = {
   locale: {
     en,
   },
 
-  random({ num = 6, language = "en" }: { num?: number; language: "en" }) {
+  random({
+    num = 6,
+    language = "en",
+  }: {
+    num?: number;
+    language: "en";
+  }): GenshinImpactCharacter[] {
     const data = this.locale[language];
 
-    return randoms(data, num);
+    return randoms(data, num).map((item) => ({
+      ...item,
+      avatar: getAssetsSrc("genshin-impact-character", item.id),
+    }));
   },
 };
 
