426530fded9578383d1016b3d8ea3ba114a2f195
meetqy <meetqy@icloud.com>
2024-08-27 17:40:50 +0800

    add template

--- apps/nextjs/src/app/kit/random-genshin-impact-character-generator/page.tsx
+++ apps/nextjs/src/app/kit/random-genshin-impact-character-generator/page.tsx
@@ -1,9 +1,18 @@
+import { Metadata } from "next";
+
 import KitLayout from "~/components/kit-layout";
+
+const name = "Genshin Impact Character";
+
+export const metadata: Metadata = {
+  title: `Random ${name} Generator`,
+  description: `Instantly generate a random ${name} with just one click!`,
+};
 
 export default function GenshinImpactCharacterPage() {
   return (
     <KitLayout
-      title="Genshin Impact Character"
+      title={name}
       readme={
         <>
           <h1>这是一段 README</h1>
--- apps/nextjs/src/components/footer.tsx
+++ apps/nextjs/src/components/footer.tsx
@@ -25,7 +25,7 @@ const navigation = {
         <svg fill="currentColor" viewBox="0 0 15 15" {...props}>
           <path
             fill="currentColor"
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M0 4h15v7H7v2H4v-2H0zm4 6V5H1v5h1V6h1v4zm1-5v7h1v-2h2V5zm4 0v5h1V6h1v4h1V6h1v4h1V5zM6 9V6h1v3z"
             clip-rule="evenodd"
           />
